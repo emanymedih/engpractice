d880217c882e0cd0a8f6dfe40fe0b7adbfae240c
emanymedih <himpest@gmail.com>
2026-07-24 11:43:40 +0300

    Align service-worker asset versions with v11.1.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='wordmemo-v11.1.1-2026-07-24';
-const CORE=['./','./index.html','./app-core.html','./v11.css?v=11.1.0','./v11.js?v=11.1.0','./v11.1-fixes.js?v=11.1.0','./diagnostics.html','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='wordmemo-v11.1.2-2026-07-24';
+const CORE=['./','./index.html','./app-core.html','./v11.css?v=11.1.1','./v11.js?v=11.1.1','./v11.1-fixes.js?v=11.1.1','./diagnostics.html','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
